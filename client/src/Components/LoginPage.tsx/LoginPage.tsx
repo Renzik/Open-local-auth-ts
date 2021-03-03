47891d0811ae5aa4f75d8435367942e608601c9e
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2021-03-03 17:24:26 -0400

    finishing OAuth buttons styles

--- client/src/Components/LoginPage.tsx/LoginPage.tsx
+++ client/src/Components/LoginPage.tsx/LoginPage.tsx
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './LoginPage.module.css';
 
 import google from '../../assets/googleLogo.png';
+import githubBlack from '../../assets/githubLogoBlack.png';
+import githubWhite from '../../assets/githubLogoWhite.png';
+import twitter from '../../assets/twitterLogo.png';
+
+import CustomButton from '../CustomButton/CustomButton';
 
 const LoginPage = () => {
+  const [whiteLogo, setWhiteLogo] = useState(false);
+  console.log(whiteLogo);
+
   const googleLogin = () => {
     window.open('http://localhost:4000/auth/google', '_self');
   };
+
+  const githubLogin = () => {};
+  const twitterLogin = () => {};
 
   return (
     <div className={styles.loginPage}>
       <div className={styles.loginForm}>
         <h1>Login</h1>
-        <div className={styles.googleContainer} onClick={googleLogin}>
-          <p>Sign up with Google</p>
-        </div>
+        <CustomButton imageUrl={google} name='Google' onClick={googleLogin}>
+          Google
+        </CustomButton>
+        <CustomButton
+          toggleLogo={setWhiteLogo}
+          imageUrl={whiteLogo ? githubWhite : githubBlack}
+          name='Github'
+          onClick={githubLogin}>
+          Github
+        </CustomButton>
+        <CustomButton imageUrl={twitter} name='Twitter' onClick={twitterLogin}>
+          Twitter
+        </CustomButton>
       </div>
     </div>
   );
